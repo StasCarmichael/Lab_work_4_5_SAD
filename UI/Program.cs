56f2547bb5891005e54a5e8926c72c6f19704f64
Stas_Kyrei <staskirey1@gmail.com>
2022-05-04 13:05:55 +0300

    ALL WORKED, added class diagram. ADD test program.

--- UI/Program.cs
+++ UI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.IO;
 
 using DAL;
 using BLL.Entity;
@@ -11,18 +12,89 @@ using UoW.Repository;
 using UoW.UnitWork;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace UI
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string BASE_CONNECTION_STRING = "Data Source=appDataBase.sqlite";
+
+
+        private static string GetStringConnection()
+        {
+            try
+            {
+                // установка пути к текущему каталогу
+                var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory());
+                // получаем конфигурацию из файла appsettings.json  создаем конфигурацию
+                var config = builder.AddJsonFile("appsettings.json").Build();
+                // получаем строку подключения
+                return config.GetConnectionString("DefaultConnection");
+            }
+            catch (Exception)
+            {
+                return BASE_CONNECTION_STRING;
+            }
+        }
+        private static void SyncroniceDataAfterDelete(ref UnitOfWork unitOfWork, DbContext context)
+        {
+            unitOfWork.Save();
+
+            unitOfWork = new UnitOfWork(context);
+        }
+
+
+
+        private static void Main(string[] args)
         {
             Console.InputEncoding = Encoding.Unicode;
             Console.OutputEncoding = Encoding.Unicode;
 
+            Console.WriteLine("\t\t\t\tDeveloped by STAS KYREI");
+            Console.WriteLine("Вас вітає програма для емуляції роботи з сутністю Антикафе !!!");
 
-            using (ApplicationContext context = new ApplicationContext())
+            Console.WriteLine("1 - Провести комплекну емуляцію");
+            Console.WriteLine("2 - Переглянути дані");
+
+            Console.Write("Введіть потрібну клавішу : ");
+            var keyCoe = Console.ReadKey().Key;
+
+
+            Console.WriteLine("\n****************************************************");
+            string stringConnection = GetStringConnection();
+
+            switch (keyCoe)
+            {
+                case (ConsoleKey.D1):
+                    {
+                        FirstTask(stringConnection);
+                        SecondTask(stringConnection);
+
+                        Console.ForegroundColor = ConsoleColor.Green ;
+                        Console.WriteLine("\n\n#######################################################\n");
+                        Console.ResetColor();
+
+                        ThirtTask(stringConnection);
+                        SecondTask(stringConnection);
+
+                        break;
+                    }
+                case (ConsoleKey.D2):
+                    {
+                        SecondTask(stringConnection);
+                        break;
+                    }
+                default:
+                    { break; }
+            }
+
+        }
+
+
+        private static void FirstTask(string stringConnection)
+        {
+            using (ApplicationContext context = new ApplicationContext(stringConnection))
             {
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
@@ -33,16 +105,23 @@ namespace UI
                 var clients = unitOfWork.Clients;
 
                 Anticafe anticafe1 = new Anticafe("Somerset", "Kiev Chrechatic 32 Б");
+                Anticafe anticafe2 = new Anticafe("Somerset", "Odesa Derevacovskaya 22");
 
 
                 Client client1 = new Client("Stas", "Kyrei", 300);
                 Client client2 = new Client("Elic", "Wise", 400);
                 Client client3 = new Client("Berni", "Clai", 200);
+                Client client4 = new Client("Alfred", "Low", 3000);
+                Client client5 = new Client("Anna", "Vercin");
 
 
                 var res1 = anticafe1.AddRestroom("Перегляд кіно", 200, 8, 22);
                 var res2 = anticafe1.AddRestroom("Настільні ігри", 50, 9, 20);
                 var res3 = anticafe1.AddRestroom("Домашня читальня", 10, 10, 20);
+
+                var res4 = anticafe2.AddRestroom("Клуб по інтересам", 20, 10, 19);
+                var res5 = anticafe2.AddRestroom("Клуб рукотворів", 40, 8, 20);
+
 
                 res1.restroom?.ReserveRestroom(client1, DateTime.Now, 8, 20);
                 res2.restroom?.ReserveRestroom(client1, new DateTime(2022, 5, 4), 9, 12);
@@ -53,40 +132,84 @@ namespace UI
                 res1.restroom?.ReserveSpecialProgramRestroom(client2, "День народження", new DateTime(2022, 5, 7));
 
 
-                clients.Add(client1, client2, client3);
-                anticafes.Add(anticafe1);
+                res4.restroom?.ReserveSpecialProgramRestroom(client5, "Корпоративне свято", new DateTime(2022, 5, 10));
+                res5.restroom?.ReserveRestroom(client1, new DateTime(2022, 5, 4), 9, 12);
+                res5.restroom?.ReserveRestroom(client5, new DateTime(2022, 5, 13), 14, 20);
+                res3.restroom?.ReserveRestroom(client5, new DateTime(2022, 5, 6), 12, 14);
+                res4.restroom?.ReserveRestroom(client4, new DateTime(2022, 5, 8), 10, 15);
+                res4.restroom?.ReserveRestroom(client3, new DateTime(2022, 5, 8), 15, 18);
+                res5.restroom?.ReserveSpecialProgramRestroom(client4, "Відне заннятя для всіх бажаючих", new DateTime(2022, 5, 12));
+
+
+                clients.Add(client1, client2, client3, client4, client5);
+                anticafes.Add(anticafe1, anticafe2);
 
                 unitOfWork.Save();
             }
-
-
-            using (ApplicationContext context = new ApplicationContext())
+        }
+        private static void SecondTask(string stringConnection)
+        {
+            using (ApplicationContext context = new ApplicationContext(stringConnection))
             {
                 UnitOfWork unitOfWork = new UnitOfWork(context);
 
-                var anticafes = unitOfWork.Anticafes.Get().ToList();
-                var restrooms = unitOfWork.Restrooms.Get().ToList();
-                var orders = unitOfWork.Orders.Get().ToList();
-                var clients = unitOfWork.Clients.Get().ToList();
+                var data = GetData(unitOfWork);
 
-
-                PrintAnricafes(anticafes);
+                PrintAnricafes(data.anticafes);
             }
+        }
+        private static void ThirtTask(string stringConnection)
+        {
+            using (ApplicationContext context = new ApplicationContext(stringConnection))
+            {
+                UnitOfWork unitOfWork = new UnitOfWork(context);
+
+                var data = GetData(unitOfWork);
+
+                var anticafe1 = unitOfWork.Anticafes.Get(1);
+                anticafe1.RemoveRestroom(anticafe1.Restrooms.FirstOrDefault());
 
 
+                SyncroniceDataAfterDelete(ref unitOfWork , context);
+                data = GetData(unitOfWork);
+
+
+                anticafe1 = unitOfWork.Anticafes.Get(2);
+                anticafe1.RemoveRestroom(anticafe1.Restrooms.FirstOrDefault());
+
+
+                SyncroniceDataAfterDelete(ref unitOfWork, context);
+                data = GetData(unitOfWork);
+
+
+                anticafe1 = unitOfWork.Anticafes.Get(1);
+                anticafe1.Restrooms.FirstOrDefault().UnreserveRestroom(anticafe1.Restrooms.FirstOrDefault().Orders.FirstOrDefault().Id);
+
+                SyncroniceDataAfterDelete(ref unitOfWork, context);
+                data = GetData(unitOfWork);
+
+                unitOfWork.Save();
+            }
         }
 
 
 
-        static void PrintAnricafes(List<Anticafe> anticafes)
+        private static (List<Anticafe> anticafes, List<Restroom> restrooms, List<Order> orders, List<Client> clients1) GetData(UnitOfWork unitOfWork)
         {
-            string anticafePass = "///////////////////////////////////////";
-            string restroomPass = "+++++++++++++++++++++++++++++++++++++++";
-            string orderPass = "========================================";
+            return (unitOfWork.Anticafes.Get().ToList(), unitOfWork.Restrooms.Get().ToList(),
+                unitOfWork.Orders.Get().ToList(), unitOfWork.Clients.Get().ToList());
+        }
+
+        private static void PrintAnricafes(List<Anticafe> anticafes)
+        {
+            string anticafePass = "//////////////////////////////////////////////";
+            string restroomPass = "++++++++++++++++++++++++++++++++++++++++++++++";
+            string orderPass = "==============================================";
 
 
             foreach (var anticafe in anticafes)
             {
+                Console.WriteLine("\n" + anticafePass);
                 Console.WriteLine("Anticafe Id = " + anticafe.Id);
                 Console.WriteLine("Name = " + anticafe.Name);
                 Console.WriteLine("Address = " + anticafe.Address);
